0de32ad9e4045606a6ad4745f094e16a4dafd03f
mahdi <hantous.mehdi2@gmail.com>
2019-07-11 12:40:15 +0100

    first commit

---
+++ sportsfit/Header.php
@@ -0,0 +1,61 @@
+	<!-- Header -->
+
+	<header class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="header_content d-flex flex-row align-items-center justify-content-start trans_400">
+						<a href="#">
+							<div class="logo d-flex flex-row align-items-center justify-content-start"><img src="images/dot.png" alt=""><div>Sport<span>fit</span></div></div>
+						</a>
+						<nav class="main_nav">
+							<ul class="d-flex flex-row align-items-center justify-content-start">
+								<li class="active"><a href="index.php">Home</a></li>
+								<li><a href="about.php">About us</a></li>
+								<li><a href="services.php">Classes & Services</a></li>
+								<li><a href="blog.php">Blog</a></li>
+								<li><a href="contact.php">Contact</a></li>
+							</ul>
+						</nav>
+						<div class="phone d-flex flex-row align-items-center justify-content-start ml-auto">
+							<div ><a href="SignIn.php">Sign In</a></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<!-- Hamburger -->
+	
+	<div class="hamburger_bar trans_400 d-flex flex-row align-items-center justify-content-start">
+		<div class="hamburger">
+			<div class="menu_toggle d-flex flex-row align-items-center justify-content-start">
+				<span>menu</span>
+				<div class="hamburger_container">
+					<div class="menu_hamburger">
+						<div class="line_1 hamburger_lines" style="transform: matrix(1, 0, 0, 1, 0, 0);"></div>
+						<div class="line_2 hamburger_lines" style="visibility: inherit; opacity: 1;"></div>
+						<div class="line_3 hamburger_lines" style="transform: matrix(1, 0, 0, 1, 0, 0);"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Menu -->
+
+	<div class="menu trans_800">
+		<div class="menu_content d-flex flex-column align-items-center justify-content-center text-center">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="about.php">About us</a></li>
+				<li><a href="services.php">Classes & Services</a></li>
+				<li><a href="blog.php">Blog</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+		</div>
+		<div class="menu_phone d-flex flex-row align-items-center justify-content-start">
+				<span ><a href="SignIn.php">Sign In</a></span>
+		</div>
+	</div>
